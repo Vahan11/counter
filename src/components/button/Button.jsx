b94bbd1d7cb3f8260b8621290b1b75953c0946d6
Vahan11 <vahansargsyan1990@gmail.com>
2021-07-25 03:32:02 +0400

    refactor: optimizations

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,14 +1,9 @@
-import React, {Component} from "react";
-import sytyle from "./Button.module.css";
+import style from "./Button.module.css";
 
-class Button extends Component {
-    render() {
-        return (
-            <div className={sytyle.btn}>
-                <button disabled={this.props.isDisabled} onClick={this.props.clicked}>{this.props.btnName}</button>
-            </div>
-        )
-    }
+export default function Button(props) {
+    return (
+        <div className={style.btn}>
+            <button disabled={props.isDisabled} onClick={props.clicked}>{props.btnName}</button>
+        </div>
+    )
 }
-
-export default Button;
